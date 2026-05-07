c2fe1a9a56fb81f31b845ea6eff6d94571dbcf42
Jonathan Wagner <dustyhair@gmail.com>
2026-05-07 03:03:04 +0000

    vmadm: expose bhyve virtio1 option

--- src/vm/tests/test-update-bhyve.js
+++ src/vm/tests/test-update-bhyve.js
@@ -163,6 +163,7 @@ var simple_properties = [
     ['alias', 'useless VM'],
     ['billing_id', '9.99'],
     ['bhyve_hostbridge', 'q35'],
+    ['virtio1', true],
     ['hostname', 'hamburgerhelper'],
     ['owner_uuid', '36bf401a-28ef-11e1-b4a7-c344deb1a5d6'],
     // Bhyve only properties.
